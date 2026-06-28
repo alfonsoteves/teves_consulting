6e32a05dc2a6467855dd4280e44fb09b1986eb4c
Alfonso Teves <alteves@hotmail.com>
2026-06-28 17:41:30 -0400

    Updates

--- src/teves_consulting_frontend/vite.config.js
+++ src/teves_consulting_frontend/vite.config.js
@@ -28,7 +28,6 @@ export default defineConfig({
   plugins: [
     react(),
     environment("all", { prefix: "CANISTER_" }),
-    environment("all", { prefix: "DFX_" }),
   ],
   resolve: {
     alias: [
